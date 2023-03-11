8a72793d416b499bedff8d0b8fb67de8de69fa43
outerLeitmotiv <olivier.mouttet@gmail.com>
2023-03-11 10:41:31 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
+<script setup>
+  import { ref } from "vue"
+  const count = ref(0);
+  const addToCount = () => { 
+  count.value = count.value+1
+  }
+  const substractToCount = () => {
+    count.value = count.value-1
+  }
+
+</script>
+
 <template>
   <main>
     <div>
-      <h4>The current count is .... </h4>
+      <h4>The current count is : </h4>
       <h1>{{ count }}</h1>
-      <button @click="count++">+</button>
-      <button @click="count--">-</button>
+      <button @click="addToCount">+</button>
+      <button @click="substractToCount">-</button>
     </div>
   </main>
 </template>
 
-<script>
-  export default {
-    data() {
-      return {
-        count: 0
-      }
-    }
-  }
-</script>
-
-<style>
+<style scoped>
   main {
-    background-color: antiquewhite;
+    background-color: hsl(183, 65%, 84%);
     height: 100vh;
     width: 100vw;
     display: flex;
@@ -31,4 +33,10 @@
   div {
     text-align: center;
   }
+
+  button {
+    margin: 4px;
+    padding: 4px;
+  }
+
 </style>
